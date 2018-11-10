7e256916bb4d330a0c0fec47383882551f39d1b6
jsparadacelis <parada.celis@gmail.com>
2018-11-10 12:27:00 -0500

    Some info in view root of API

--- api/urls.py
+++ api/urls.py
@@ -3,21 +3,24 @@ from .views import transaccion, cuenta, perfil, cliente, root
 
 urlpatterns = [
     #Urls for cuenta
-    path('cuenta/', cuenta.CuentaList.as_view()), 
-    path('cuenta/<int:pk>', cuenta.CuentaDetail.as_view()),
-    #For adding money in account
-    path('add_money/', cuenta.add_money),
-    path('set_zero/', cuenta.set_zero),
-    path("list_tran/<int:id>", cuenta.list_tran),
-    #Urls for transaccion
-    path('transaccion/', transaccion.TransaccionList.as_view()),
-    path('transaccion/<int:pk>', transaccion.TransaccionDetail.as_view()),
-    #Urls for perfil
-    path('perfil/', perfil.PerfilList.as_view()),
-    path('perfil/<int:pk>', perfil.PerfilDetail.as_view()),
-    #Urls for cliente
-    path('cliente/', cliente.ClienteList.as_view()),
-    path('cliente/<int:pk>', cliente.ClienteDetail.as_view()),
+    path('cuenta/', cuenta.CuentaList.as_view(), name = 'cuenta'), 
+    path('cuenta/<int:pk>', cuenta.CuentaDetail.as_view(), name = 'cuenta_id'),
 
-    path("", include(root.router.urls) , name="")
+    path('add_money/', cuenta.add_money, name = 'add_money'),
+    path('set_zero/', cuenta.set_zero, name = 'set_zero'),
+    path("list_tran/<int:id>", cuenta.list_tran, name = 'list_tran'),
+
+    #Urls for transaccion
+    path('transaccion/', transaccion.TransaccionList.as_view(), name = 'transaccion'),
+    path('transaccion/<int:pk>', transaccion.TransaccionDetail.as_view(), name = 'transaccion_id'),
+
+    #Urls for perfil
+    path('perfil/', perfil.PerfilList.as_view(), name = 'perfil'),
+    path('perfil/<int:pk>', perfil.PerfilDetail.as_view(), name = 'perfil_id'),
+    
+    #Urls for cliente
+    path('cliente/', cliente.ClienteList.as_view(), name = 'cliente'),
+    path('cliente/<int:pk>', cliente.ClienteDetail.as_view(), name = 'cliente_id'),
+
+    path("", root.view_root , name="root")
 ]
--- api/views/root.py
+++ api/views/root.py
@@ -1,9 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-from rest_framework import routers
-from . import transaccion, cuenta, perfil, cliente, root
 
-router = routers.DefaultRouter()
-#
-    
+def view_root(request):
+    return render(request, 'index.htm')
