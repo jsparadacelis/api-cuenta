6f2de4364c6ddd48dbd75585b620c4c283414c5e
jsparadacelis <parada.celis@gmail.com>
2018-11-14 12:12:26 -0500

    Change settings for DB

--- api_cuenta/settings.py
+++ api_cuenta/settings.py
@@ -106,6 +106,8 @@ DATABASES = {
     }
 }
 
+DATABASES = {'default': dj_database_url.config()}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
