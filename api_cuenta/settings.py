b6df8afc5a0e86fca6b175dabbc6c7c8b134de2e
jsparadacelis <parada.celis@gmail.com>
2018-11-14 12:49:27 -0500

    Validation for environment

--- api_cuenta/settings.py
+++ api_cuenta/settings.py
@@ -114,17 +114,6 @@ else:
         }
     } 
 
-
-''' DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-} '''
-
-
-
-
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
