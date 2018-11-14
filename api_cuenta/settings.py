ee7fd56c9f325b5baf0048f7274033d6e9d1c7d1
jsparadacelis <parada.celis@gmail.com>
2018-11-13 22:25:39 -0500

    Change DB engine to postgres

--- api_cuenta/settings.py
+++ api_cuenta/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'pol)3pp0bn)-tw$iph%d%*405j7l7b%z4&wdtra0iw)ngp32^m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
