0a057199608a8b836c796c43f39ca9d686da59ee
jsparadacelis <parada.celis@gmail.com>
2018-11-14 12:21:21 -0500

    Change settings for DB

--- api_cuenta/settings.py
+++ api_cuenta/settings.py
@@ -88,6 +88,33 @@ WSGI_APPLICATION = 'api_cuenta.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+
+ON_HEROKU = os.environ.get('ON_HEROKU')
+HEROKU_SERVER = os.environ.get('HEROKU_SERVER')
+
+if ON_HEROKU:
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'api_cuenta',
+            'USER': 'api_user',
+            'PASSWORD': 'admin123',
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
+    }
+
+    DATABASES = {'default': dj_database_url.config()}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    } 
+
+
 ''' DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -95,18 +122,7 @@ WSGI_APPLICATION = 'api_cuenta.wsgi.application'
     }
 } '''
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'api_cuenta',
-        'USER': 'api_user',
-        'PASSWORD': 'admin123',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
 
-DATABASES = {'default': dj_database_url.config()}
 
 
 # Password validation
