085a96ac9651a42213ad74e5c2322791ce4a708f
jsparadacelis <parada.celis@gmail.com>
2018-11-21 20:59:49 -0500

    Cedula's field unique

--- cliente/models.py
+++ cliente/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Cliente (models.Model):
     nombre = models.CharField(max_length=50)
     apellido = models.CharField(max_length=50)
-    cedula = models.IntegerField()
+    cedula = models.IntegerField(unique = True)
 
     def __str__(self):
         return self.nombre
