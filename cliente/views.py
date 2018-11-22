ab1d20cbd29db9c70770a71f5f4036bed78c2952
jsparadacelis <parada.celis@gmail.com>
2018-11-21 20:53:04 -0500

    Import status from DRF

--- cliente/views.py
+++ cliente/views.py
@@ -1,5 +1,5 @@
 #DRF Utilities
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
